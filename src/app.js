97a8e9bf2ac51a1e08664f1a08c1c38beb7ab46a
SebastianLozanoSiza <jsebastianlozano1207@outlook.com>
2025-03-30 22:53:14 -0500

    enrutador y controladores

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 /* Importamos al framework express */
 import express from 'express';
+import categoriaRoutes from './routes/categorias.routes.js';
 
 /* Asignamos a app toda funcionalidad para mi server web */
 const app = express();
 
 /* Setear un puerto a mi web server */
 app.set("port", 5000);
+
+/* routes */
+app.use("/api/categorias", categoriaRoutes)
 
 /* Hacemos disponible a mi server app para toda la aplicación */
 export default app;
